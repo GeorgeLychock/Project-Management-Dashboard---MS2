61e28f44055bb3122300c2c4b9730faeeccecc07
George <glychock@gmail.com>
2021-02-15 14:03:47 -0500

    feature: Created On and Off buttons to add a widget panel from the library to the active widget viewport

--- js/script.js
+++ js/script.js
@@ -1,62 +1,17 @@
-function getData(url, cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", url);
-    xhr.send();
+function turnWidgetOn() {
+    this.title = "Project One Due Dates";
+    this.progress = "80";
+    this.blockers = "2";
+    return $("#active-widgets-data").append(`<div><h3>${title}</h3><p>Percent Progress: ${progress}%</p><p>Blockers: ${blockers}</p>`);
 }
 
-function getTableHeaders(obj) {
-    var tableHeaders = [];
-
-    Object.keys(obj).forEach(function(key) {
-        tableHeaders.push(`<td>${key}</td>`);
-    });
-
-    return `<tr>${tableHeaders}</tr>`;
+function turnWidgetOff() {
+    this.title = "Project One Due Dates";
+    this.progress = "80";
+    this.blockers = "2";
+    return $("#active-widgets-data").html("");
 }
 
-function generatePaginationButtons(next, prev) {
-    if (next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button>
-                <button onclick="writeToDocument('${next}')">Next</button>`;
-    } else if (next && !prev) {
-        return `<button onclick="writeToDocument('${next}')">Next</button>`;
-    } else if (!next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
-    }
-}
-
-function writeToDocument(url) {
-    var tableRows = [];
-    var el = document.getElementById("data");
-
-    getData(url, function(data) {
-        var pagination = "";
-
-        if (data.next || data.previous) {
-            pagination = generatePaginationButtons(data.next, data.previous);
-        }
-        data = data.results;
-        var tableHeaders = getTableHeaders(data[0]);
-
-        data.forEach(function(item) {
-            var dataRow = [];
-
-            Object.keys(item).forEach(function(key) {
-                var rowData = item[key].toString();
-                var truncatedData = rowData.substring(0, 15);
-                dataRow.push(`<td>${truncatedData}</td>`);
-            });
-            tableRows.push(`<tr>${dataRow}</tr>`);
-        });
-
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
-    });
-}
-
+/*    this.view = function () {
+    $("#active-widgets-data").append(`<div><h3>${title}</h3><p>Percent Progress: ${progress}%</p><p>Blockers: ${blockers}</p>`);
+    } */
