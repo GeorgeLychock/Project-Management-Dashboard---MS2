8d4630dc68fdcf6d9d951e00a7c902b4356d7941
George <glychock@gmail.com>
2021-03-08 23:12:11 -0500

    scripts: Renamed all vars used in project dashboard build to include 'project'

--- js/script.js
+++ js/script.js
@@ -9,38 +9,38 @@ createActiveProjects();
 })
 function buildProjectPanelMarkup(data) {
 
-    var widgetData = data;
+    var projectData = data;
 
     /* CODE REUSE - Progress Bar below is from Bootstrap Documentation: https://getbootstrap.com/docs/4.6/components/progress/  */
-    return `<div id="${widgetData.widgetID}" class="col-3 pmd-max-height-400">
+    return `<div id="${projectData.widgetID}" class="col-3 pmd-max-height-400">
         <div class="pmd-panel-head">
-        <button class="pmd-icon-01" onclick="turnWidgetOff('${widgetData.widgetID}')"><i class="bi bi-arrow-right-square pmd-acolor-1" aria-hidden="true"></i></button>
+        <button class="pmd-icon-01" onclick="turnProjectOff('${projectData.widgetID}')"><i class="bi bi-arrow-right-square pmd-acolor-1" aria-hidden="true"></i></button>
         </div>
         <div class="pmd-active-widget-3col pmd-bcolor-2">
-            <h5>${widgetData.name}</h5>
-            <div>Project Due Date:${widgetData.duedate}</div>
-            <div>Project Owner:${widgetData.owner}</div>
-            <div>${widgetData.description}</div>
+            <h5>${projectData.name}</h5>
+            <div>Project Due Date:${projectData.duedate}</div>
+            <div>Project Owner:${projectData.owner}</div>
+            <div>${projectData.description}</div>
             <div class="progress pmd-prg-row">
-                <div class="progress-bar pmd-prg-bar" role="progressbar" aria-valuenow="${widgetData.percentcomplete}" aria-valuemin="0" aria-valuemax="100"></div>
+                <div class="progress-bar pmd-prg-bar" role="progressbar" aria-valuenow="${projectData.percentcomplete}" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
             <div class="progress">
-                <div class="progress-bar pmd-prg-bar" role="progressbar" style="width: 25%" aria-valuenow="${widgetData.cpi}" aria-valuemin="0" aria-valuemax="2"></div>
+                <div class="progress-bar pmd-prg-bar" role="progressbar" style="width: 25%" aria-valuenow="${projectData.cpi}" aria-valuemin="0" aria-valuemax="2"></div>
             </div>
             <div class="progress">
-                <div class="progress-bar pmd-prg-bar" role="progressbar" style="width: 50%" aria-valuenow="${widgetData.sv}" aria-valuemin="0" aria-valuemax="2"></div>
+                <div class="progress-bar pmd-prg-bar" role="progressbar" style="width: 50%" aria-valuenow="${projectData.sv}" aria-valuemin="0" aria-valuemax="2"></div>
             </div>
-            <div><a href="${widgetData.liveSite}" target="_blank">Development Site Link</a></div>
+            <div><a href="${projectData.liveSite}" target="_blank">Development Site Link</a></div>
         </div>
     </div>`;
 }
 
 function buildLibraryButtonMarkup(data) {
 
-    var widgetData = data;
+    var projectData = data;
 
     return `<div class="pmd-btn-library pmd-btncolor-1" id="widget-btn-${data.widgetID}">
-    <button class="pmd-icon-01" onclick="turnWidgetOn('${data.widgetID}')">
+    <button class="pmd-icon-01" onclick="turnProjectOn('${data.widgetID}')">
     <i class="bi bi-arrow-left pmd-acolor-2" aria-hidden="true"></i>
     </button>
     <div class="pmd-dinline pmd-acolor-1">${data.name}</div>
@@ -48,18 +48,18 @@ function buildLibraryButtonMarkup(data) {
 }
 
 function createActiveProjects() {
-    if (localStorage.localWidgets) {
+    if (localStorage.localProjects) {
 
-        let activeWidgetsSaved = localStorage.getItem('localWidgets');
-        let activeWidgets = activeWidgetsSaved.split(',');
+        let activeProjectsSaved = localStorage.getItem('localProjects');
+        let activeProjects = activeProjectsSaved.split(',');
         let elementID;
 
-        for (let i in activeWidgets) {
+        for (let i in activeProjects) {
 
-            var url = "http://www.georgelychock-career.com/pages/_sandbox/ms2/data/" + activeWidgets[i] + ".json";
+            var url = "http://www.georgelychock-career.com/pages/_sandbox/ms2/data/" + activeProjects[i] + ".json";
             getData(url, function (data) {
-                var widgetData = data;
-                elementID = widgetData.widgetID;
+                var projectData = data;
+                elementID = projectData.widgetID;
                 return $("#active-projects-data").append(buildProjectPanelMarkup(data));
             });
         }
@@ -70,28 +70,28 @@ function createLibraryButtons() {
 
     // Retrieve widget IDs that have already been activated to the dashboard, if available. Display only buttons for widgets that have not been activated to the dashbaord
 
-    if (localStorage.localWidgets) {
+    if (localStorage.localProjects) {
 
         //*1 Search " *Foot Note 1 " in Technical Constraints section of README.md
 
         var widgetIDs = ["proj0001", "proj0002", "proj0003", "proj0004", "proj0005"];
-        let activeWidgetsSaved = localStorage.getItem('localWidgets');
-        var widgetIDsSaved = activeWidgetsSaved.split(',');
-        var widgetBuildIDs = [];
+        let activeProjectsSaved = localStorage.getItem('localProjects');
+        var projectIDsSaved = activeProjectsSaved.split(',');
+        var projectBuildIDs = [];
 
         //Search the locally stored active widget IDs and make an array of available widgets that are not already active
 
         for (let i in widgetIDs) {
-            if (widgetIDsSaved.includes(widgetIDs[i]) == false) {
-                widgetBuildIDs.push(widgetIDs[i]);
+            if (projectIDsSaved.includes(widgetIDs[i]) == false) {
+                projectBuildIDs.push(widgetIDs[i]);
             }
         }
     } else {
-        var widgetBuildIDs = ["proj0001", "proj0002", "proj0003", "proj0004", "proj0005"]; // else all widgets are available in the library
+        var projectBuildIDs = ["proj0001", "proj0002", "proj0003", "proj0004", "proj0005"]; // else all widgets are available in the library
     }
 
-    for (let i in widgetBuildIDs) {
-        var url = "http://www.georgelychock-career.com/pages/_sandbox/ms2/data/" + widgetBuildIDs[i] + ".json";
+    for (let i in projectBuildIDs) {
+        var url = "http://www.georgelychock-career.com/pages/_sandbox/ms2/data/" + projectBuildIDs[i] + ".json";
         //Build buttons
         getData(url, function(data) {
             return $("#projects-library").append(buildLibraryButtonMarkup(data));
@@ -144,30 +144,30 @@ function storageAvailable(type) {
 /* /CODE REUSE - MDN https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API  */
 
 /* Widget Library ON/OFF Buttons */
-function turnWidgetOn(widgetIdOn) {
+function turnProjectOn(widgetIdOn) {
 
     let elementID = widgetIdOn;
     var url = "http://www.georgelychock-career.com/pages/_sandbox/ms2/data/" + elementID + ".json";
 
     getData(url, function(data) {
 
-        let widgetData = data;
+        let projectData = data;
 
         // Check if localStorage is enabled
         if (storageAvailable('localStorage')) {
             // YES: We can use localStorage, check if localStorage var has been initiated
-            if (localStorage.localWidgets) {
+            if (localStorage.localProjects) {
                 // YES: Add widget ID to the localStorage string and store
-                let activeWidgetsSaved = localStorage.getItem('localWidgets'); //returns a string, comma delimited
-                let activeWidgets = activeWidgetsSaved.split(',');
-                activeWidgets.push(elementID);
-                localStorage.setItem('localWidgets', activeWidgets); //stored as a string, comma delimited
+                let activeProjectsSaved = localStorage.getItem('localProjects'); //returns a string, comma delimited
+                let activeProjects = activeProjectsSaved.split(',');
+                activeProjects.push(elementID);
+                localStorage.setItem('localProjects', activeProjects); //stored as a string, comma delimited
 
             } else {
 
-                let activeWidgets = [];
-                activeWidgets.push(elementID);
-                localStorage.setItem('localWidgets', activeWidgets);
+                let activeProjects = [];
+                activeProjects.push(elementID);
+                localStorage.setItem('localProjects', activeProjects);
             }
         } else {
             // NO: no localStorage
@@ -179,13 +179,13 @@ function turnWidgetOn(widgetIdOn) {
     });
 }
 
-function turnWidgetOff(widgetIdOff) {
+function turnProjectOff(widgetIdOff) {
     var elementID = widgetIdOff;
     // remove widget from localStorage
-    let activeWidgetsSaved = localStorage.getItem('localWidgets');
-    let activeWidgets = activeWidgetsSaved.split(',');
-    activeWidgets.pop(elementID);
-    localStorage.setItem('localWidgets', activeWidgets);
+    let activeProjectsSaved = localStorage.getItem('localProjects');
+    let activeProjects = activeProjectsSaved.split(',');
+    activeProjects.pop(elementID);
+    localStorage.setItem('localProjects', activeProjects);
 
     // **** This should be replaced by a function, see duplication in buildLibraryButtons(), Build Button and add to Library
     var url = "http://www.georgelychock-career.com/pages/_sandbox/ms2/data/" + elementID + ".json";
