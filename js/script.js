16fafd4ea284a7148b2b95efc0f53d49aac7dd76
George <glychock@gmail.com>
2021-05-26 21:46:06 -0400

    scripts: Changing the fetch routin to include no-cors, Test #1

--- js/script.js
+++ js/script.js
@@ -12,14 +12,17 @@ const JSONFolderURL = "https://github.com/GeorgeLychock/Project-Management-Dashb
 
     /* Get data from JSON file  */
     /* CODE REUSE - XMLHttpRequest Callback, Code Institute, jQuery/API Module  */
-function getData(url) {
-    let retrievedData;
-    fetch(url)
-        .then(response => response.json())
-        .then(retrievedData => JSON.parse(retrievedData));
-        console.log("this is the retrieved data from fetch" + retrievedData);
-        return retrievedData;
-    }
+    async function getData(url) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          mode: 'no-cors', // no-cors, *cors, same-origin
+        });
+
+            console.log("This is the fetched data: " + response.json());
+
+        return response.json(); // parses JSON response into native JavaScript objects
+      }
     /* /CODE REUSE - XMLHttpRequest Callback, Code Institute, jQuery/API Module  */
 
     /* Checks to make sure localStorage is usuable in the browser */
