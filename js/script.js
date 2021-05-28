22a41611c7aa4f69db9ac7f79209820180481678
George <glychock@gmail.com>
2021-05-27 22:00:46 -0400

    scripts: Working on fixing the GitHub Pages issues with fetch JSON; TEST #2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
 })
 
 /* CONSTANTS */
-const JSONFolderURL = "https://www.georgelychock-career.com/pages/_sandbox/ms2/data/";
+const JSONFolderURL = "https://github.com/GeorgeLychock/Project-Management-Dashboard---MS2/blob/master/data/";
+// const JSONFolderURL = "http://www.georgelychock-career.com/pages/_sandbox/ms2/data/";
 
 /* ******* REUSED JAVASCRIPT ********** */
 
